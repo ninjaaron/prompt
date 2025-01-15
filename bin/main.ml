bf8593c17d927ac589ae1ed526aab56057c3e5c4
Aaron Christianson <ninjaaron@gmail.com>
2025-01-15 09:55:48 +0100

    tightened things up

--- bin/main.ml
+++ bin/main.ml
@@ -1,51 +1,32 @@
 open StdLabels
-
-let home = Re.Perl.compile_pat (Sys.getenv "HOME")
+let home_pat = Re.Perl.compile_pat ("^" ^ (Sys.getenv "HOME"))
+let path_part = Re.Perl.compile_pat "(.).*?/"
+let active_branch = Re.Perl.compile_pat {|\* (.*?)\n|}
 
 let get_short_dir cwd =
-  let cwd' = Re.replace_string home ~by:"~" cwd in
-  let rec loop = function
-      [] -> failwith "cwd should not be empty"
-    | [_] as tail -> tail
-    | "" :: tail -> "" :: loop tail
-    | head :: tail ->
-      let len = if head.[0] = '.' then 2 else 1 in
-      String.sub head ~pos:0 ~len :: loop tail in
-  loop (String.split_on_char ~sep:'/' cwd')
-  |> String.concat ~sep:"/"
-
-let active_pat = Re.Perl.compile_pat {|\* (.*)|}
-
-let rec get_active_branch = function
-    [] -> Error "output was empty"
-  | hd :: tl ->
-    match Re.exec_opt active_pat hd with
-      Some group -> Ok (Re.Group.get group 1)
-    | None -> get_active_branch tl
-
+  Re.replace_string home_pat ~by:"~" cwd |>
+  Re.replace path_part ~f:(fun g -> Re.Group.get g 1 ^ "/")
+    
+let get_active_branch s =
+  Re.(Group.get (exec active_branch s) 1)
+  
 let get_git_prompt () =
   let open Subprocess.Results in
-  let lines args = cmd args |> devnull_err |> lines |> string_error in
-  let* branch_out = lines ["git"; "branch"] in
-  let* branch = get_active_branch branch_out in
-  let* status_out = lines ["git"; "status"; "-s"] in
-  let color = if status_out = [] then "green" else "red" in
+  let* branch_out = cmd ["git"; "branch"] |> devnull_err |> read in
+  let* status_out = cmd ["git"; "status"; "-s"] |> devnull_err |> read in
+  let branch = get_active_branch branch_out in
+  let color = if status_out = "" then "green" else "red" in
   Ok (color, branch)
 
 let get_updates () =
   let fh = (Sys.getenv "HOME") ^ "/.updates" |> open_in in
-  let rec loop total =
-    try loop ((total * 256) + input_byte fh)
-    with End_of_file -> total
-  in loop 0
+  let f total c = int_of_char c + (256 * total) in
+  String.fold_left ~init:0 ~f (In_channel.input_all fh)
 
 let get_update_prompt () =
-  try
-    match get_updates () with
-      0 -> None
-    | n -> Some ("%F{yellow}" ^ Printf.sprintf "%x" n ^ "%f|")
-  with Sys_error _ -> None
-    
+  match get_updates () with
+  | exception Sys_error _ | 0 -> None
+  | n -> Some ("%F{yellow}" ^ Printf.sprintf "%x" n ^ "%f|")
 
 let () =
   if Array.length Sys.argv > 1 && Sys.argv.(1) = "-t" then
@@ -55,26 +36,23 @@ let () =
     (print_endline "%F{yellow}%m%f:%F{red}%~%f# ";
      exit 0);
 
-  let dir_prompt =
+  let (let*) = Option.bind in
+  let dir =
     let short_dir = get_short_dir (Sys.getcwd ()) in
     Some (String.concat ~sep:"" ["%F{blue}"; short_dir; "%f> "])
 
-  and host_prompt =
-    Sys.getenv_opt "SSH_TTY"
-    |> Option.map (fun _ -> "%F{green}%m%f:")
+  and host =
+    let* _ = Sys.getenv_opt "SSH_TTY" in Some "%F{green}%m%f:"
 
-  and git_prompt =
-    get_git_prompt () |> Result.to_option |> Option.map
-      (fun (color, branch) ->
-         String.concat ~sep:"" ["%F{"; color; "}"; branch; "%f|"])
+  and git =
+    let* color, branch = get_git_prompt () |> Result.to_option in
+    Some (String.concat ~sep:"" ["%F{"; color; "}"; branch; "%f|"])
 
-  and venv = Sys.getenv_opt "VIRTUAL_ENV"
-           |> Option.map (fun venv -> Filename.basename venv ^ "|")
+  and venv =
+    let* venv = Sys.getenv_opt "VIRTUAL_ENV" in
+    Some (Filename.basename venv ^ "|")
 
-  and update_prompt = get_update_prompt ()
-  in
-  let prompt =
-    List.filter_map
-      ~f:Fun.id [venv; update_prompt; git_prompt; host_prompt; dir_prompt]
-    |> String.concat ~sep:"" in
-  print_endline prompt;
+  and update = get_update_prompt () in
+
+  print_endline @@ String.concat ~sep:""
+  @@ List.filter_map ~f:Fun.id [venv; update; git; host; dir]
