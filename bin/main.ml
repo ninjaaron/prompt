b81578cdb407fbc4c069123baca58787aedcd6ed
Aaron Christiason <ninjaaron@gmail.com>
2025-03-30 19:21:51 +0200

    seeing how it feels to write the prompt with managed_in

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,5 @@
 open StdLabels
-module Unix = UnixLabels
+let (|-) = Fun.compose
 
 let home_pat = Re.Perl.compile_pat ("^" ^ (Sys.getenv "HOME"))
 let path_part = Re.Perl.compile_pat "(.).*?/"
@@ -9,22 +9,21 @@ let get_short_dir cwd =
   Re.replace_string home_pat ~by:"~" cwd |>
   Re.replace path_part ~f:(fun g -> Re.Group.get g 1 ^ "/")
 
-let proc_out args =
-  let stdout, stdin, stderr =
-    Unix.open_process_args_full args.(0) args [||] in
-  let lines = In_channel.input_all stdout in
-  match Unix.close_process_full (stdout, stdin, stderr) with
-  | Unix.WEXITED 0 -> Some lines
-  | _ -> None
+let get_git_prompt branch_ic status_ic =
+  let open Subprocess.Results in
+  match managed_read branch_ic, managed_read status_ic with
+  | Error _, _ | _, Error _ -> None
+  | Ok branch_out, Ok status_out ->
+    let branch = Re.(Group.get (exec active_branch branch_out) 1) in
+    let color = if status_out = "" then "green" else "red" in
+    Some (color, branch)
 
-let get_git_prompt () =
-  let (let*) = Option.bind in
-  let* branch_out = proc_out [|"git"; "branch"|] in
-  let* branch_g = Re.exec_opt active_branch branch_out in
-  let branch = Re.Group.get branch_g 1 in
-  let* status_out = proc_out [|"git"; "status"; "-s"|] in
-  let color = if status_out = "" then "green" else "red" in
-  Some (color, branch)
+let get_git_prompt_later () =
+  let open Subprocess in
+  let open' = open_out |- devnull_err |- cmd in
+  let branch_ic = open' ["git"; "branch"] in
+  let status_ic = open' ["git"; "status"; "-s"] in
+  fun () -> get_git_prompt branch_ic status_ic
 
 let get_updates () =
   let fh = (Sys.getenv "HOME") ^ "/.updates" |> open_in in
@@ -37,31 +36,23 @@ let get_update_prompt () =
   | n -> Some ("%F{yellow}" ^ Printf.sprintf "%x" n ^ "%f|")
 
 let () =
-  if Array.length Sys.argv > 1 && Sys.argv.(1) = "-t" then
-    exit 0;
-
   if Sys.getenv "USER" = "root" then
     (print_endline "%F{yellow}%m%f:%F{red}%~%f# ";
      exit 0);
+  let get_git_prompt = get_git_prompt_later () in
 
-  let (let*) = Option.bind in
+  let (let+) opt f = Option.map f opt in
   let dir =
     let short_dir = get_short_dir (Sys.getcwd ()) in
     Some (String.concat ~sep:"" ["%F{blue}"; short_dir; "%f> "])
-
   and host =
-    let* _ = Sys.getenv_opt "SSH_TTY" in Some "%F{green}%m%f:"
-
+    let+ _ = Sys.getenv_opt "SSH_TTY" in "%F{green}%m%f:"
   and git =
-    get_git_prompt () |> Option.map
-      (fun (color, branch) ->
-         String.concat ~sep:"" ["%F{"; color; "}"; branch; "%f|"])
-
+    let+ color, branch = get_git_prompt () in
+    String.concat ~sep:"" ["%F{"; color; "}"; branch; "%f|"]
   and venv =
-    let* venv = Sys.getenv_opt "VIRTUAL_ENV" in
-    Some (Filename.basename venv ^ "|")
-
+    let+ venv = Sys.getenv_opt "VIRTUAL_ENV" in
+    Filename.basename venv ^ "|"
   and update = get_update_prompt () in
-
   print_endline @@ String.concat ~sep:""
   @@ List.filter_map ~f:Fun.id [venv; update; git; host; dir]
