88cef4e44e2637f5c0c240a267e0f540393d5f8e
Aaron Christiason <ninjaaron@gmail.com>
2025-03-30 20:11:19 +0200

    tiny refactors

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,8 @@
 open StdLabels
-let (|-) = Fun.compose
+open Sys
 
-let home_pat = Re.Perl.compile_pat ("^" ^ (Sys.getenv "HOME"))
+let home = getenv "HOME"
+let home_pat = Re.Perl.compile_pat ("^" ^ home)
 let path_part = Re.Perl.compile_pat "(.).*?/"
 let active_branch = Re.Perl.compile_pat {|\* (.*?)\n|}
 
@@ -20,27 +21,27 @@ let get_git_prompt branch_ic status_ic =
 
 let get_git_prompt_later () =
   let open Subprocess in
-  let open' = open_out |- devnull_err |- cmd in
+  let open' c = open_out @@ devnull_err @@ cmd c in
   let branch_ic = open' ["git"; "branch"] in
   let status_ic = open' ["git"; "status"; "-s"] in
   fun () -> get_git_prompt branch_ic status_ic
 
 let get_updates () =
-  let fn = (Sys.getenv "HOME") ^ "/.updates" in
-  if not (Sys.file_exists fn) then None else
+  let fn = home ^ "/.updates" in
+  if not (file_exists fn) then None else
     let fh = open_in fn in
     let f total c = int_of_char c + (256 * total) in
     Some (String.fold_left ~init:0 ~f (In_channel.input_all fh))
 
 let () =
-  match Sys.getenv "USER" with
+  match getenv "USER" with
   | "root" -> print_endline "%F{yellow}%m%f:%F{red}%~%f# "
   | _ ->
     let get_git_prompt = get_git_prompt_later () in
     let (let+) opt f = Option.map f opt in
-    let dir = let short_dir = get_short_dir (Sys.getcwd ()) in
+    let dir = let short_dir = get_short_dir @@ getcwd () in
       Some (String.concat ~sep:"" ["%F{blue}"; short_dir; "%f> "])
-    and host = let+ _ = Sys.getenv_opt "SSH_TTY" in "%F{green}%m%f:"
+    and host = let+ _ = getenv_opt "SSH_TTY" in "%F{green}%m%f:"
     and git = let+ color, branch = get_git_prompt () in
       String.concat ~sep:"" ["%F{"; color; "}"; branch; "%f|"]
     and venv = let+ venv = Sys.getenv_opt "VIRTUAL_ENV" in
