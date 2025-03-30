b43b311af746c13bfe54340ed91f11d4f12faec9
Aaron Christiason <ninjaaron@gmail.com>
2025-03-30 19:50:08 +0200

    tiny refactors

--- bin/main.ml
+++ bin/main.ml
@@ -26,33 +26,26 @@ let get_git_prompt_later () =
   fun () -> get_git_prompt branch_ic status_ic
 
 let get_updates () =
-  let fh = (Sys.getenv "HOME") ^ "/.updates" |> open_in in
-  let f total c = int_of_char c + (256 * total) in
-  String.fold_left ~init:0 ~f (In_channel.input_all fh)
-
-let get_update_prompt () =
-  match get_updates () with
-  | exception Sys_error _ | 0 -> None
-  | n -> Some ("%F{yellow}" ^ Printf.sprintf "%x" n ^ "%f|")
+  let fn = (Sys.getenv "HOME") ^ "/.updates" in
+  if not (Sys.file_exists fn) then None else
+    let fh = open_in fn in
+    let f total c = int_of_char c + (256 * total) in
+    Some (String.fold_left ~init:0 ~f (In_channel.input_all fh))
 
 let () =
-  if Sys.getenv "USER" = "root" then
-    (print_endline "%F{yellow}%m%f:%F{red}%~%f# ";
-     exit 0);
-  let get_git_prompt = get_git_prompt_later () in
-
-  let (let+) opt f = Option.map f opt in
-  let dir =
-    let short_dir = get_short_dir (Sys.getcwd ()) in
-    Some (String.concat ~sep:"" ["%F{blue}"; short_dir; "%f> "])
-  and host =
-    let+ _ = Sys.getenv_opt "SSH_TTY" in "%F{green}%m%f:"
-  and git =
-    let+ color, branch = get_git_prompt () in
-    String.concat ~sep:"" ["%F{"; color; "}"; branch; "%f|"]
-  and venv =
-    let+ venv = Sys.getenv_opt "VIRTUAL_ENV" in
-    Filename.basename venv ^ "|"
-  and update = get_update_prompt () in
-  print_endline @@ String.concat ~sep:""
-  @@ List.filter_map ~f:Fun.id [venv; update; git; host; dir]
+  match Sys.getenv "USER" with
+  | "root" -> print_endline "%F{yellow}%m%f:%F{red}%~%f# "
+  | _ ->
+    let get_git_prompt = get_git_prompt_later () in
+    let (let+) opt f = Option.map f opt in
+    let dir = let short_dir = get_short_dir (Sys.getcwd ()) in
+      Some (String.concat ~sep:"" ["%F{blue}"; short_dir; "%f> "])
+    and host = let+ _ = Sys.getenv_opt "SSH_TTY" in "%F{green}%m%f:"
+    and git = let+ color, branch = get_git_prompt () in
+      String.concat ~sep:"" ["%F{"; color; "}"; branch; "%f|"]
+    and venv = let+ venv = Sys.getenv_opt "VIRTUAL_ENV" in
+      Filename.basename venv ^ "|"
+    and update = let+ n = get_updates () in
+      "%F{yellow}" ^ Printf.sprintf "%x" n ^ "%f|" in
+    print_endline @@ String.concat ~sep:""
+    @@ List.filter_map ~f:Fun.id [venv; update; git; host; dir]
